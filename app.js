c4c981bdc9ec1168a8df179b63df28f3b67aa5bf
Irina Safarova <irinasfv19@gmail.com>
2023-04-20 18:20:32 +0300

    refactor app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
+
 const app = express();
 
 // Код ошибки "Такой страницы не существует"
@@ -13,7 +13,6 @@ const { PORT = 3000 } = process.env;
 const { userRouter, cardRouter } = require('./routes');
 
 app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 // Подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb');
